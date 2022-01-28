e6dbf657f88e133a7cfa79d293efe4023c066652
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2022-01-28 20:37:45 +0300

    Update Ex02_1AverageCalculation.java

--- src/me/mila/lesson02/Ex02_1AverageCalculation.java
+++ src/me/mila/lesson02/Ex02_1AverageCalculation.java
@@ -1,7 +1,7 @@
 package me.mila.lesson02;
 
 /* Пользователь вводит через консоль два числа. Найти их среднее арифметическое. */
-
+//Delete all the comments! Code should be written in such way that we can undestand it without any additional explanations!
 import java.util.Scanner;
 
 public class Ex02_1AverageCalculation {
@@ -14,9 +14,10 @@ public class Ex02_1AverageCalculation {
 
         System.out.print("Input b: ");       //ввести b
         double b = in.nextInt();             //инициализация дробного значения b, перевод на новую строку
-
+//arithmetical mean is always division by 2, so you do not need this 'm'. However, bad naming in this case.
         int m = 2;                           //присвоили значение m
         double c = (a + b) / m;              //вывод дробного значения с
+        //actually, we do not need to close Scanner in our case, there will be no resource leaks. Why did you do that?
         in.close();                          //завершение scanner
         System.out.println("Result с: " + c); //вывод результата c
 
